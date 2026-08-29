21ae2e67fd4b08610d06fb2fb2f6c26d8b2e29e9
fredydlemus <fredyflemus@gmail.com>
2026-08-29 11:52:40 -0600

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "equipment-sme-assistant"
+}
